73c35ebd8f0a9ab851f3ac466dcfcf37369f7af1
long.nguyen <long.nguyen@uni-bielefeld.de>
2022-12-29 16:56:08 +0100

    Deploy on GH Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -15,6 +15,9 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
+		paths: {
+			base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
+		},
 	},
 };
 
