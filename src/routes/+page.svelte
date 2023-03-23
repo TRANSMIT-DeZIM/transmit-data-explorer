aa0d799246ff10f4aa084b0d24deb27980004ddb
long.nguyen <long.nguyen@uni-bielefeld.de>
2023-03-23 19:40:12 +0100

    Add modal for project description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -186,8 +186,14 @@
 		<label for="page-drawer" class="drawer-overlay" />
 
 		<div class="menu w-80 bg-base-100 p-4 pr-8 text-base-content">
-			<div class="p-4">
-				<img src={logo} alt="TRANSMIT Project logo" />
+			<div class="flex p-4">
+				<label
+					for="about-modal"
+					class="tooltip tooltip-bottom cursor-pointer"
+					data-tip="About the project"
+				>
+					<img class="hover:blur-sm" src={logo} alt="TRANSMIT Project logo" />
+				</label>
 			</div>
 
 			<div class="mt-4">
@@ -199,6 +205,46 @@
 					>
 				{/each}
 			</div>
+		</div>
+	</div>
+
+	<input type="checkbox" id="about-modal" class="modal-toggle" />
+	<div class="modal">
+		<div class="modal-box relative w-5/6 max-w-5xl">
+			<label for="about-modal" class="btn-sm btn-circle btn absolute right-2 top-2">✕</label>
+			<h3 class="text-lg font-bold">
+				Transnational Perspectives on Migration and Integration (TRANSMIT)
+			</h3>
+			<p class="py-4">
+				<a
+					class="link"
+					target="_blank"
+					rel="noreferrer"
+					href="https://www.dezim-institut.de/projekte/projekt-detail/transnational-perspectives-on-migration-and-integration-transmit-7-13/"
+					>Transnational Perspectives on Migration and Integration (TRANSMIT)</a
+				>
+				is an infrastructure project of the
+				<a
+					class="link"
+					target="_blank"
+					rel="noreferrer"
+					href="https://www.dezim-institut.de/dezim-forschungsgemeinschaft/ueber-die-forschungsgemeinschaft/"
+					>DeZIM-Forschungsgemeinschaft (DeZIM FG)</a
+				> and funded by the German Federal Ministry for Family Affairs, Senior Citizens, Women and Youth
+				(BMFSFJ). The project studies the complex interplay between migration dynamics, migrants' well-being
+				and integration processes by building a transnational data infrastructure that collects and links
+				quantitative and qualitative data and knowledge on countries of origin, transit and destination.
+				In addition to Turkey and Lebanon, TRANSMIT collects data in Lebanon, Morocco, Italy, Nigeria,
+				Senegal, the Gambia, and Germany.
+			</p>
+			<p class="py-4">
+				The data presented in the web application are part of an ongoing longitudinal survey of
+				Syrian nationals in Turkey and Lebanon, collected by the BIM and the IAB in a joint research
+				project on Transnational Migration and Integration (TRANSMIT). The study surveys a
+				nationally representative sample of the respective Syrians in the two countries, as well as
+				a sample of the host population living in the same neighborhoods. In the absence of reliable
+				registry data, the surveys rely on stratified area sampling and random walk techniques.
+			</p>
 		</div>
 	</div>
 </div>
