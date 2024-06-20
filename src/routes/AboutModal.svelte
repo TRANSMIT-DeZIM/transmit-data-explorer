10a4025db7e203df893e2492ba768fe3482586cd
Long <hoanglong.nguyen@outlook.de>
2024-06-20 15:46:03 +0200

    Separate About modal component

---
+++ src/routes/AboutModal.svelte
@@ -0,0 +1,60 @@
+<input type="checkbox" id="about-modal" class="modal-toggle" />
+<div class="modal">
+	<div class="modal-box relative w-5/6 max-w-5xl">
+		<label for="about-modal" class="btn btn-circle btn-sm absolute right-2 top-2">✕</label>
+		<h2 class="text-xl font-bold">
+			Transnational Perspectives on Migration and Integration (TRANSMIT)
+		</h2>
+		<p class="pt-4">
+			<a
+				class="link"
+				target="_blank"
+				rel="noreferrer"
+				href="https://www.dezim-institut.de/projekte/projekt-detail/transnational-perspectives-on-migration-and-integration-transmit-7-13/"
+				>Transnational Perspectives on Migration and Integration (TRANSMIT)</a
+			>
+			is an infrastructure project of the
+			<a
+				class="link"
+				target="_blank"
+				rel="noreferrer"
+				href="https://www.dezim-institut.de/dezim-forschungsgemeinschaft/ueber-die-forschungsgemeinschaft/"
+				>DeZIM-Forschungsgemeinschaft (DeZIM FG)</a
+			> and funded by the German Federal Ministry for Family Affairs, Senior Citizens, Women and Youth
+			(BMFSFJ). The project studies the complex interplay between migration dynamics, migrants' well-being
+			and integration processes by building a transnational data infrastructure that collects and links
+			quantitative and qualitative data and knowledge on countries of origin, transit and destination.
+			In addition to Turkey and Lebanon, TRANSMIT collects data in Lebanon, Morocco, Italy, Nigeria,
+			Senegal, the Gambia, and Germany.
+		</p>
+		<p class="pt-4">
+			The data presented in the web application are part of an ongoing longitudinal survey of
+			Syrian nationals in Turkey and Lebanon, collected by the BIM and the IAB in a joint research
+			project on Transnational Migration and Integration (TRANSMIT). The study surveys a
+			nationally representative sample of the respective Syrians in the two countries, as well as
+			a sample of the host population living in the same neighborhoods. In the absence of reliable
+			registry data, the surveys rely on stratified area sampling and random walk techniques.
+		</p>
+		<p class="pt-4">
+			The web application was developed by <a
+				class="link"
+				target="_blank"
+				rel="noreferrer"
+				href="https://lo-ng.netlify.app/">Long Nguyen</a
+			>
+			(<a
+				class="link"
+				target="_blank"
+				rel="noreferrer"
+				href="https://github.com/TRANSMIT-DeZIM/transmit-data-explorer/">source code</a
+			>). The content is licensed under a
+			<a
+				class="link"
+				target="_blank"
+				rel="noreferrer"
+				href="https://creativecommons.org/licenses/by/4.0/"
+				>Creative Commons Attribution 4.0 International license</a
+			>.
+		</p>
+	</div>
+</div>
