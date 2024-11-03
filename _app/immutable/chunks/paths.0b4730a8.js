a7d82164cf36790a22368643c41a5e036e1ddc84
Long <hoanglong.nguyen@outlook.de>
2024-11-03 22:25:31 +0100

    Updates

--- _app/immutable/chunks/paths.0b4730a8.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_yygo9l)==null?void 0:s.base)??"/transmit-data-explorer";var a;const e=((a=globalThis.__sveltekit_yygo9l)==null?void 0:a.assets)??t;export{e as a,t as b};
